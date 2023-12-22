60fc5d0c9a08f12ec283bff5b17c3d9a256c687f
Adrian Miller <adrian.m.miller@gmail.com>
2023-12-23 05:35:17 +1100

    Initial commit

---
+++ set.sh
@@ -0,0 +1,9 @@
+git init --initial-branch=main
+sleep 2
+git remote add origin https://gitlab.com/adrian.m.miller/hide-my-applist-guide.git
+sleep 2
+git add .
+sleep 2
+git commit -m "Initial commit"
+sleep 2
+git push --set-upstream origin main
